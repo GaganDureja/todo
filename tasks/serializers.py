1b44a3bb0038ecb75f6baaf170bd93278c27c7ff
GaganDureja <gagandureja675@gmail.com>
2024-06-28 17:54:13 +0530

    due date validation added

--- tasks/serializers.py
+++ tasks/serializers.py
@@ -1,7 +1,13 @@
 from rest_framework import serializers
 from .models import Tasks
+from datetime import date
 
 class TasksSerializer(serializers.ModelSerializer):
   class Meta:
     model = Tasks
     fields = '__all__'
+
+  def validate_due_date(self, value):
+    if value and value < date.today():
+      raise serializers.ValidationError("Due date should be a future date.")
+    return value
