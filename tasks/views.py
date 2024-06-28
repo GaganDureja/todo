0fc8744eb7f55ab639f86201dd1fbd3d122cac49
GaganDureja <gagandureja675@gmail.com>
2024-06-28 17:47:46 +0530

    task crud operations added

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import generics
+from .models import Tasks
+from .serializers import TasksSerializer
+
+class TasksListCreateAPIView(generics.ListCreateAPIView):
+  queryset = Tasks.objects.all()
+  serializer_class = TasksSerializer
+
+class TasksRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
+  queryset = Tasks.objects.all()
+  serializer_class = TasksSerializer
